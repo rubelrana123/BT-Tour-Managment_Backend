64c83a4f08c354197dd043b7e3fc68b66e13dd47
rubelrana123 <rubelranatpi707458@gmail.com>
2025-07-19 00:19:17 +0600

    create division & tour interface and schema

---
+++ src/app/modules/tour/tour.model.ts
@@ -0,0 +1,32 @@
+import { model, Schema, Types } from "mongoose";
+import { ITour, ITourType } from "./tour.interface";
+const tourTypeSchema = new Schema<ITourType>({
+    name: { type: String, required: true , unique : true },
+
+},{timestamps : true})
+export const TourType = model<ITourType>("TourType", tourTypeSchema);
+
+const tourSchema = new Schema<ITour>({
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String },
+    images:{ type: [String], default : [] },
+    location:  { type: String },
+    costFrom: { type: Number },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    included: { type: [String], default : [] },
+    excluded: { type: [String], default : [] },
+    amenities: { type: [String], default : [] },
+    tourPlan: { type: [String], default : [] },
+    maxGuest: { type: Number },
+    minAge: { type: Number },
+    division: { type: Schema.ObjectId, ref : "Division", required: true   },
+    tourType: { type: Schema.ObjectId, ref : "TourType",  required: true }
+
+},
+{
+    timestamps : true
+})
+
+export const Tour = model<ITour>("Tour", tourSchema)
